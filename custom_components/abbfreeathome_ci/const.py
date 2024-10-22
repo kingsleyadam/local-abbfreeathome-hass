1af44ab5abaf6afd3c55e3f3aa6921ca90f7aa4f
Adam Kingsley <adam@kingsley.io>
2024-10-22 12:01:05 +0200

    Add options to include/exclude orphan nodes

--- custom_components/abbfreeathome_ci/const.py
+++ custom_components/abbfreeathome_ci/const.py
@@ -1,6 +1,7 @@
 """Constants for the ABB free@home integration."""
 
-DOMAIN = "abbfreeathome_ci"
+DOMAIN = "abbfreeathome"
 
 # Domain Configuration
 CONF_SERIAL = "serial"
+CONF_INCLUDE_ORPHAN_CHANNELS = "include_orphan_channels"
