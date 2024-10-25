301477796b48cd3075809522a2712a973323b330
derjoerg <derjoerg@users.noreply.github.com>
2024-10-25 12:15:48 +0200

    Implement Trigger as Button (#52)
    * Implement Trigger as Button
    
    * Removed callback registration

---
+++ custom_components/abbfreeathome_ci/button.py
@@ -0,0 +1,64 @@
+"""Create ABB Free@Home button entities."""
+
+from abbfreeathome.devices.trigger import Trigger
+from abbfreeathome.freeathome import FreeAtHome
+
+from homeassistant.components.button import ButtonEntity, ButtonEntityDescription
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.device_registry import DeviceInfo
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+from .const import CONF_SERIAL, DOMAIN
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Set up buttons."""
+    free_at_home: FreeAtHome = hass.data[DOMAIN][entry.entry_id]
+
+    async_add_entities(
+        FreeAtHomeButtonEntity(button, sysap_serial_number=entry.data[CONF_SERIAL])
+        for button in free_at_home.get_device_by_class(device_class=Trigger)
+    )
+
+
+class FreeAtHomeButtonEntity(ButtonEntity):
+    """Defines a Free@Home button entity."""
+
+    _attr_should_poll: bool = False
+
+    def __init__(self, button: Trigger, sysap_serial_number: str) -> None:
+        """Initialize the button."""
+        super().__init__()
+        self._button = button
+        self._sysap_serial_number = sysap_serial_number
+
+        self.entity_description = ButtonEntityDescription(
+            key="button",
+            name=button.channel_name,
+        )
+
+    @property
+    def device_info(self) -> DeviceInfo:
+        """Information about this entity/device."""
+        return {
+            "identifiers": {(DOMAIN, self._button.device_id)},
+            "name": self._button.device_name,
+            "manufacturer": "ABB busch-jaeger",
+            "serial_number": self._button.device_id,
+            "suggested_area": self._button.room_name,
+            "via_device": (DOMAIN, self._sysap_serial_number),
+        }
+
+    @property
+    def unique_id(self) -> str | None:
+        """Return a unique ID."""
+        return f"{self._button.device_id}_{self._button.channel_id}_button"
+
+    async def async_press(self) -> None:
+        """Press the button."""
+        await self._button.press()
