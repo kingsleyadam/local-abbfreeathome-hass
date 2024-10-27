d807514ffec3924807468ad5c8856f42dcf3484e
derjoerg <derjoerg@users.noreply.github.com>
2024-10-27 20:24:26 +0100

    Add parameter-names to Diagnostics (#58)
    * Add parameters to diagnostics
    
    * Reordered code
    
    * Fixed typo
    
    * Linting

--- custom_components/abbfreeathome_ci/diagnostics.py
+++ custom_components/abbfreeathome_ci/diagnostics.py
@@ -7,6 +7,7 @@ from typing import Any
 
 from abbfreeathome.bin.function import Function
 from abbfreeathome.bin.pairing import Pairing
+from abbfreeathome.bin.parameter import Parameter
 from abbfreeathome.freeathome import FreeAtHome
 
 from homeassistant.components.diagnostics import async_redact_data
@@ -18,21 +19,24 @@ from .const import DOMAIN
 TO_REDACT = {"latitude", "longitude", "sysapName", "uartSerialNumber"}
 
 
-def inject_function_pairing_names(device_list: list[dict]):
-    """Inject the function and pairing names into the list of devices."""
+def inject_function_pairing_parameter_names(device_list: list[dict]):
+    """Inject the function, pairaing and parameter names into the list of devices."""
+
     for _device_value in device_list.values():
-        for _channel_key, _channel_value in _device_value.get("channels").items():
+        _device_value["parameterNames"] = {}
+        for _device_param_key, _device_param_value in _device_value.get(
+            "parameters"
+        ).items():
             try:
-                _channel_value["function"] = Function(
-                    int(_channel_value.get("functionID"), 16)
-                ).name
-            except ValueError:
-                _channel_value["function"] = "UNKNOWN"
+                _device_value["parameterNames"][
+                    f"{Parameter(int(_device_param_key.lstrip("par"), 16)).name} ({_device_param_key})"
+                ] = _device_param_value
+            except ValueError:  # noqa: PERF203
+                _device_value["parameterNames"][f"UNKNOWN ({_device_param_key})"] = (
+                    _device_param_value
+                )
 
-            _device_value["channels"][_channel_key] = OrderedDict(
-                sorted(_channel_value.items())
-            )
-
+        for _channel_key, _channel_value in _device_value.get("channels").items():
             for _input_key, _input_value in _channel_value.get("inputs").items():
                 try:
                     _input_value["pairing"] = Pairing(
@@ -57,6 +61,30 @@ def inject_function_pairing_names(device_list: list[dict]):
                     sorted(_output_value.items())
                 )
 
+            _channel_value["parameterNames"] = {}
+            for _channel_param_key, _channel_param_value in _channel_value.get(
+                "parameters"
+            ).items():
+                try:
+                    _channel_value["parameterNames"][
+                        f"{Parameter(int(_channel_param_key.lstrip("par"), 16)).name} ({_channel_param_key})"
+                    ] = _channel_param_value
+                except ValueError:  # noqa: PERF203
+                    _channel_value["parameterNames"][
+                        f"UNKNOWN ({_channel_param_key})"
+                    ] = _channel_param_value
+
+            try:
+                _channel_value["function"] = Function(
+                    int(_channel_value.get("functionID"), 16)
+                ).name
+            except ValueError:
+                _channel_value["function"] = "UNKNOWN"
+
+            _device_value["channels"][_channel_key] = OrderedDict(
+                sorted(_channel_value.items())
+            )
+
 
 async def async_get_config_entry_diagnostics(
     hass: HomeAssistant, entry: ConfigEntry
@@ -65,6 +93,8 @@ async def async_get_config_entry_diagnostics(
     _free_at_home: FreeAtHome = hass.data[DOMAIN][entry.entry_id]
 
     # Inject Function and Pairing names into configuration.
-    inject_function_pairing_names((await _free_at_home.get_config()).get("devices"))
+    inject_function_pairing_parameter_names(
+        (await _free_at_home.get_config()).get("devices")
+    )
 
     return async_redact_data(await _free_at_home.get_config(), TO_REDACT)
