7175096e1f7a9c732013c37b94d5cadc7bfd9fad
Joerg Schoppet <joerg@schoppet.de>
2024-11-16 00:05:22 +0100

    Implement cover entity

--- custom_components/abbfreeathome_ci/__init__.py
+++ custom_components/abbfreeathome_ci/__init__.py
@@ -20,6 +20,7 @@ _LOGGER = logging.getLogger(__name__)
 PLATFORMS: list[Platform] = [
     Platform.BINARY_SENSOR,
     Platform.BUTTON,
+    Platform.COVER,
     Platform.CLIMATE,
     Platform.EVENT,
     Platform.LIGHT,
