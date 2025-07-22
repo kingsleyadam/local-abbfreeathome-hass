f931e51db33162315e552c16501db244c5481023
Joerg Schoppet <joerg@schoppet.de>
2025-07-22 12:39:26 +0200

    First try

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Tests for the abbfreeathome_ci integration."""
---
+++ tests/conftest.py
@@ -0,0 +1,4 @@
+"""Fixtures for testing."""
+import pytest
+
+from pytest_homeassistant_custom_component.syrupy import HomeAssistantSnapshotExtension
